f296433752dad2228b3d94dfe73741c3d97355ff
Antonio Ruiz <antonio.ruiz@wmich.edu>
2017-11-29 11:38:10 -0500

    SPIKES ON SHINY

---
+++ Auto/app.R
@@ -0,0 +1,72 @@
+library(shiny)
+library(MASS)
+
+data <- Auto
+View(data)
+attach(data)
+
+str(data)
+
+
+summary(data)
+max(data$year)
+
+
+ui <- fluidPage(
+  titlePanel("AUTO DATASET"),
+  
+  fluidRow(
+    column(4,
+           selectInput("model",
+                       "Model:",
+                       c("All",
+                         unique(as.character(data$name))))
+    ),
+    
+    column(4,
+           selectInput("cyl",
+                       "Cylinders:",
+                       c("All",
+                         unique(as.character(data$cylinders))))
+    ),
+    
+    column(4,
+           sliderInput("year", "Year:",
+                       min = min(data$year), max = max(data$year),
+                       value = 1, step = 1,
+                       animate =
+                         animationOptions(interval = 1, loop = TRUE))
+           
+           
+    )
+  ),
+  
+  fluidRow(
+    DT::dataTableOutput("table")
+  )
+)
+  
+
+  
+server<-
+  function(input, output) {
+    
+    # Filter data based on selections
+    output$table <- DT::renderDataTable(DT::datatable({
+      data <- data
+      if (input$model != "All") {
+        data <- data[data$name == input$model,]
+      }
+      if (input$cyl != "All") {
+        data <- data[data$cyl == input$cyl,]
+      }
+      if (input$year != "All") {
+        data <- data[data$year == input$year,]
+      }
+      data
+    }))
+    
+  }
+
+
+shinyApp(ui = ui, server = server)
---
+++ shiny_demo.R
@@ -0,0 +1,27 @@
+install.packages("shiny")
+install.packages("DT")
+
+library(shiny)
+library(DT)
+#runExample("01_hello")
+###create a mock app in current directory
+dir <- getwd()
+dir
+setwd("/home/tony/R/work/shiny/")
+
+
+runApp("App")
+runApp("Auto")
+
+
+#runExample("01_hello")      # a histogram
+#runExample("02_text")       # tables and data frames
+runExample("03_reactivity") # a reactive expression
+runExample("04_mpg")        # global variables
+runExample("05_sliders")    # slider bars
+#runExample("06_tabsets")    # tabbed panels
+#runExample("07_widgets")    # help text and submit buttons
+#runExample("08_html")       # Shiny app built from HTML
+#runExample("09_upload")     # file upload wizard
+#runExample("10_download")   # file download wizard
+#runExample("11_timer")      # an automated timer
