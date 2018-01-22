010782487455cff866d2df1c6a1b40f588fc810a
Apolinar Ortega <apolinar.ortega@wmich.edu>
2018-01-21 22:22:04 -0500

    Completed Data Download Spike

--- Spikes/Download Web Data Spikes/download.R
+++ Spikes/Download Web Data Spikes/download.R
@@ -1,36 +1,43 @@
 # Other NHANES API?
   # https://github.com/chiragjp/nhanes_scidata
   # https://pic-sure.org/products/nhanes-unified-dataset
-# NHANES List
-  # https://www.cdc.gov/nchs/nhanes/nhanes_listserv.htm
 
 
-# SrapeR Attampt
-library("XML")
-library("stringr")
-library("scrapeR")
+# NHANES Data Listing
+  # https://wwwn.cdc.gov/nchs/nhanes/search/datapage.aspx
+# ScrapeR Information
   # https://cran.r-project.org/web/packages/scrapeR/scrapeR.pdf
 
-# Get HTML Page
-list.url = "https://wwwn.cdc.gov/nchs/nhanes/search/datapage.aspx"
-page.source = scrape(url=list.url, parse=F)
+library("stringr")
+library("scrapeR")
+
+# HTML page -> string
+page.source = scrape(url="https://wwwn.cdc.gov/nchs/nhanes/search/datapage.aspx", parse=F)
 source.string = gsub("[\r\n\t]", "", toString(page.source))
 
-# Captures tables and then rows from it
+# Get desired table from the page
 source.table = str_match(source.string, "<table[^>]+>(.+?)</table>")[1,2]
 source.table.rows = str_match_all(source.table, "<tr>(.+?)</tr>")[[1]]
+row.data = str_match_all(source.table.rows, "<td.+?>(.+?)</td>")
 
-row.data = str_match_all(rows, "<td.+?>(.+?)</td>")
-
-# Transform into a 2d array
-the.data = array(dim=c(length(row.data),6))
+# Downloads data
 for(i in 1:length(row.data))
 {
+  if(i %% 50 == 0)
+    print(paste0(i, "/", length(row.data)))
+  
+  # Extracts desired data
   item = row.data[[i]][,2]
-  link1 = link = str_match(item[3], "<a href=\"(.+?)\">(.+?)</a>")[,2:3]
-  link2 = link = str_match(item[4], "<a href=\"(.+?)\">(.+?)</a>")[,2:3]
-  the.data[i,] = c(item[1:2], link1, link2)
+  item = c( item[1:2],                                             # Year & Data File Name
+            str_match(item[3], "<a href=\"(.+?)\">(.+?)</a>")[,2], # Documentaiton Link
+            str_match(item[4], "<a href=\"(.+?)\">(.+?)</a>")[,2]) # Data Link
+  
+  # Download data if link available
+  if(!is.na(item[4])){
+    download.file(url      = paste0("https://wwwn.cdc.gov/", item[4]),
+                  destfile = paste0("data/", item[2], " [", item[1], "]"),
+                  method   = "curl", quiet=T)
+  }else{
+    print(paste0(i, ": ", item[2], "[", item[1], "]", " was not selected"))
+      }
 }
-
-
-#download.file("https://wwwn.cdc.gov/Nchs/Nhanes/1999-2000/ACQ.XPT", "testDownload", "curl")
