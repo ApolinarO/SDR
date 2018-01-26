ae5ecf30cb9a69e153bcecd303923c971aad5abe
Tony <antonio.ruiz@wmich.edu>
2018-01-26 17:21:29 -0500

    file upload spike updates

--- Source/File_Spike/app.R
+++ Source/File_Spike/app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(DT)
+library(dplyr)
 
 
 ui <- fluidPage(
@@ -9,8 +10,12 @@ ui <- fluidPage(
       #selectInput("col", "Select a column", character(0))
       checkboxGroupInput("columns","Select Columns",character(0)),
       actionButton("updateButton","Update View"),
+      actionButton("subset", "Subset Rows"),
       downloadButton('downloadData', 'Download')
+    
+      
     ),
+
     mainPanel(
       tableOutput("table"),
       textOutput("selected")
@@ -38,7 +43,59 @@ server <- function(input, output, session) {
     updateCheckboxGroupInput(session, "columns", choices = names(data()))
   })
   
+  #use server to get information from the table
+  observeEvent(input$subset,{
+    df <- data.frame(data()) 
+    str(df)
+    print("subsetting removes NAS")
+    df <- na.omit(df)
+    output$selected <- renderText(
+      #cols <- input$columns,
+      for (x in input$columns){
+        print(paste("this is ", x))
+        print(typeof(x)) #views ints as char??
+        col <- select(df,contains(x))
+        if(!is.na(is.numeric(x))){
+          
+          values <- select(df, contains(x))
+         # print(values)
+          print('got here')
+          #print(min(values))
+          #print(max(values))
+          output$UI_button <- insertUI(
+            selector = "#subset",
+            where = "afterEnd",
+            ui = actionButton("updateSub", "Apply Subsetting")
+
+          )
+          
+          output$UI_button <- insertUI(
+            selector = "#subset",
+            where = "afterEnd",
+               ui = sliderInput("int", x,
+                            min = min(values), max = max(values),
+                             value = c(min(values),max(values)))
+          )
+        }
+      }
+      
+    )
+    
+    
+  })
   
+  ##apply changes based on subset button
+  observeEvent(input$updateSub, {
+    ##change the data frame
+  
+    df <- data.frame(data())
+    df <- filter(df, input$int) #subsetting takes place here
+    # browser()
+    output$table <- renderTable({
+      req(df)
+      head(df)
+    })
+  })
       
   
   #update the table 
@@ -51,25 +108,7 @@ server <- function(input, output, session) {
       head(df)
     })
   })
- # 
-# observeEvent(input$downloadButton, {
-#  #df <- subset.data.frame(data(),input$columns) #subsetting takes place here
-#    output$downloadData <- downloadHandler(
-#      
-#      filename = function() {
-#        paste(data.frame(data()), ".csv", sep = "")
-#      },
-#      content = function(file) {
-#        write.csv(data.frame(data()), file, row.names = FALSE)
-#      }
-#    )
-#  })
-  
-#change_df <- function(){
-#  df <- subset(data.frame(data()), input$columns)
-#  return (df)
-#}
-  
+ 
 datasetInput <- reactive({
   
   df <- subset(data.frame(data()), select = input$columns)
